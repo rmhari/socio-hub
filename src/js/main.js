0446edd2da4a549e0a4e8b3942f6c5130e057a5a
haripriyaravichandran <rm.haripriya@gmail.com>
2024-04-05 17:29:02 +0530

    Email fallback

--- src/js/main.js
+++ src/js/main.js
@@ -5,17 +5,24 @@ const postsContainer = document.getElementById("posts");
 const commentTemplate = document.getElementById("commentTemplate");
 
 
-const renderPost = (post) => {
+const renderPost = (post,users) => {
     const postContainer = postTemplate.content.cloneNode(true);
     postContainer.querySelector("h5").innerHTML = post.title;
     postContainer.querySelector("p").innerHTML = post.body;
     const commentsCount = postContainer.querySelector("div.d-flex.flex-row.muted-color");
     const commentsContainer = postContainer.querySelector( ".comments");
+    const userName = postContainer.querySelector(".font-weight-bold");
+    const companyName = postContainer.querySelector(".text-primary");
     postsContainer.appendChild(postContainer);  
 
-  fetch('https://jsonplaceholder.typicode.com/users/' + post.userId)
-    .then(response => response.json())
-    .then(user => console.log(user))
+    const user = users.find((user) => user.id === post.userId);
+
+    userName.innerHTML = user.name;
+    companyName.innerHTML = user.company.name;
+
+    
+
+    console.log(user);
 
 
   fetch('https://jsonplaceholder.typicode.com/posts/' + post.id + '/comments')
@@ -25,7 +32,17 @@ const renderPost = (post) => {
         comments.forEach(comment => {
            const commentContainer = commentTemplate.content.cloneNode(true);
            commentContainer.querySelector(".comment-text").innerHTML = comment.body;
-           commentContainer.querySelector(".name").innerHTML = comment.email;
+           
+
+           const user = users.find((user) => user.email === comment.email);
+
+           if(user) {
+            commentContainer.querySelector(".name").innerHTML = user.name;
+           } else {
+            commentContainer.querySelector(".name").innerHTML = comment.email;
+           }
+
+           
 
            commentsContainer.appendChild(commentContainer); 
         });
@@ -33,13 +50,17 @@ const renderPost = (post) => {
         )
 }
 
-
-fetch('https://jsonplaceholder.typicode.com/posts')
+fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
-  .then(posts => {
-    posts.forEach(post => {
-        renderPost(post);
-    });
+  .then(users => {
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(posts => {
+      posts.forEach(post => {
+          renderPost(post, users);
+      });
+      
+  })
     
 })
-
